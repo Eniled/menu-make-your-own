6697dd3b239a437d0ea395d78c24961dff532ea4
Eniled <eniled.ortiz@upr.edu>
2026-07-22 00:24:08 -0400

    Actualización: arreglo de detalles

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4168123895" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1845715953" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
